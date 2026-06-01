e2e9262384f03baadf094e2f73a025c5fa4bbec8
nandini <nandini.g.gupta9.9@gmail.com>
2026-06-01 12:13:06 +0000

    update the HelperConfig.s.sol file

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {Script, console2} from "forge-std/Script.sol";
 
 abstract contract CodeConstants {
@@ -45,7 +45,9 @@ contract HelperConfig is CodeConstants, Script {
         // Note: We skip doing the local config
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].priceFeed != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
@@ -59,15 +61,21 @@ contract HelperConfig is CodeConstants, Script {
                                 CONFIGS
     //////////////////////////////////////////////////////////////*/
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
-        });
+        return
+            NetworkConfig({
+                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+            });
     }
 
-    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            priceFeed: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF // ETH / USD
-        });
+    function getZkSyncSepoliaConfig()
+        public
+        pure
+        returns (NetworkConfig memory)
+    {
+        return
+            NetworkConfig({
+                priceFeed: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF // ETH / USD
+            });
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -82,7 +90,10 @@ contract HelperConfig is CodeConstants, Script {
         console2.log(unicode"⚠️ You have deployed a mock contract!");
         console2.log("Make sure this was intentional");
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
